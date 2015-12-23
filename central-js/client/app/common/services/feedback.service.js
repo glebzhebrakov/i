3328e93c806ae65968f61c81320ee73eb0b21f97
Gleb Zhebrakov <glebzhebrakov@gmail.com>
2015-12-23 13:28:59 +0200

    #991 Реализовать страницу расширенного фидбэка по итогу оказания услуги, чтоб можно было оставлять комментарий #991

--- central-js/client/app/common/services/feedback.service.js
+++ central-js/client/app/common/services/feedback.service.js
@@ -1,18 +1,5 @@
 angular.module('app').service('FeedbackService', function ($http, $q) {
 
-  this.setMessage = function(message, userMessage) {
-    var data = {
-      "sMail": message.sMail,
-      "sHead": message.sHead,
-      "sBody": message.sBody
-    };
-
-    return $http.post('./api/messages', data).then(function(response) {
-      return response.data;
-    });
-  };
-
-
     this.getFeedback = function (id, token){
       var deferred = $q.defer();
       $http.get('./api/messages/feedback?sID_Order='+id+'&sToken='+token+'').success(function (data, status) {
@@ -33,9 +20,7 @@ angular.module('app').service('FeedbackService', function ($http, $q) {
         "sBody": body
       };
 
-      $http.post('./api/messages/feedback', data).then(function(response) {
-        return response.data;
-      });
+      $http.post('./api/messages/feedback', data).then(function(response) {});
     };
 
 });
