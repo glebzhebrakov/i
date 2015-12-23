2e8c921d8c645fd2f8708bae77a14e4e98426908
glebzhebrakov <glebzhebrakov@gmail.com>
2015-12-23 12:37:33 +0200

    #991 Реализовать страницу расширенного фидбэка по итогу оказания услуги, чтоб можно было оставлять комментарий #991

--- central-js/client/app/common/services/feedback.service.js
+++ central-js/client/app/common/services/feedback.service.js
@@ -1,17 +1,41 @@
-angular.module('app').factory('FeedbackService', function ($http, $q) {
+angular.module('app').service('FeedbackService', function ($http, $q) {
 
-  return {
+  this.setMessage = function(message, userMessage) {
+    var data = {
+      "sMail": message.sMail,
+      "sHead": message.sHead,
+      "sBody": message.sBody
+    };
 
-    getFeedback: function (id, token){
+    return $http.post('./api/messages', data).then(function(response) {
+      return response.data;
+    });
+  };
+
+
+    this.getFeedback = function (id, token){
       var deferred = $q.defer();
-
-      $http.get('./wf/message/getMessage_Feedback?sID_Order='+id+'&sToken='+token+'').success(function (data, status) {
-        deferred.resolve(data);
-      }).error(function (data, status) {
-        deferred.reject(true);
+      $http.get('./api/messages/feedback?sID_Order='+id+'&sToken='+token+'').success(function (data, status) {
+        if (data.code === 'SYSTEM_ERR' || data.code==='BUSINESS_ERR'){
+          deferred.reject(data);
+        } else {
+          deferred.resolve(data);
+        }
       });
 
       return deferred.promise;
-    }
-  }
+    };
+
+    this.postFeedback =function(id,token, body){
+      var data = {
+        "sID_Order": id,
+        "sToken": token,
+        "sBody": body
+      };
+
+      $http.post('./api/messages/feedback', data).then(function(response) {
+        return response.data;
+      });
+    };
+
 });
